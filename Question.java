d5ed5b72938317a25be078f60155f932491aedf3
Mark Joestar <mark.ianadlao@gmail.com>
2020-05-01 14:00:34 -0400

    inital commit

---
+++ Question.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Question {
+	public static void main (String[] args){ 
+
+	multiply(300, 200);
+	multiply(2, 300);
+	multiply(40, 6);
+	multiply(31, 22);
+
+
+	}
+
+	public static void multiply(int a, int b){
+		System.out.println(a * b);
+
+	}
+}
